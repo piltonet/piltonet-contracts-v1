8cc8301ee6dca8e7bcef9980e39845501a815c41
Farokh Madah <fa.madah@gmail.com>
2023-12-16 21:50:59 +0330

    move lib folder

--- contracts/decentralized-identity/DIDAccount.sol
+++ contracts/decentralized-identity/DIDAccount.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/utils/introspection/IERC165.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/interfaces/IERC1271.sol";
 import "@openzeppelin/contracts/utils/cryptography/SignatureChecker.sol";
-import "../lib/Bytecode.sol";
+import "./lib/Bytecode.sol";
 import "./interfaces/IERC6551Account.sol";
 
 /// @title Piltonet Decentralized Identity Account
