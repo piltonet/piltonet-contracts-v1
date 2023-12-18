65472b5f43ecce6a6fb9c397489dde3ea2ee20b4
Farokh Madah <fa.madah@gmail.com>
2023-12-19 02:22:49 +0330

    improve tba contracts

--- contracts/Profile.sol
+++ contracts/Profile.sol
@@ -15,6 +15,17 @@ contract Profile is ERC721, ERC721URIStorage, Ownable {
     ERC6551Registry public Registry;
     mapping(address => address) private _tba;
 
+    /*///////////////////////////////////////////////////////////////
+                            Events
+    //////////////////////////////////////////////////////////////*/
+    
+    /// @notice Emitted when a profile has been created
+    event ProfileCreated(uint256 indexed tokenId, address indexed account);
+
+    /*///////////////////////////////////////////////////////////////
+                            Constructor
+    //////////////////////////////////////////////////////////////*/
+    
     constructor(
         string memory baseURI,
         address _ERC6551Account,
@@ -26,7 +37,10 @@ contract Profile is ERC721, ERC721URIStorage, Ownable {
         Registry = ERC6551Registry(_ERC6551Registry);
     }
 
-    function createProfile(address mainAccount) public onlyOwner {
+    function createProfile(address mainAccount) public onlyOwner returns (address) {
+        // each account can create a unique profile
+        require(_tba[mainAccount] == address(0), "Error: Account has already been created!");
+
         // tokenId values start at 1
         _tokenId++;
 
@@ -42,14 +56,17 @@ contract Profile is ERC721, ERC721URIStorage, Ownable {
         // mint profilr NFT
         _safeMint(mainAccount, _tokenId);
         _setTokenURI(_tokenId, Utils.toString(_tokenBoundAccount));
+
+        // save tba in contract and return that
+        _tba[mainAccount] = _tokenBoundAccount;
+        
+        emit ProfileCreated(_tokenId, _tokenBoundAccount);
+
+        return _tokenBoundAccount;
     }
 
     function totalSupply() external view returns (uint256) {
         return _tokenId;
-    }
-
-    function _baseURI() internal view override returns (string memory) {
-        return _baseTokenURI;
     }
 
     function setBaseURI(string memory baseURI) public onlyOwner {
@@ -57,15 +74,21 @@ contract Profile is ERC721, ERC721URIStorage, Ownable {
     }
 
     // The following functions are overrides required by Solidity.
-    function tokenURI(
-        uint256 tokenId
-    ) public view override(ERC721, ERC721URIStorage) returns (string memory) {
+    
+    function _baseURI() internal view override returns (string memory) {
+        return _baseTokenURI;
+    }
+    
+    function tokenURI(uint256 tokenId) public view override(ERC721, ERC721URIStorage) returns (string memory) {
         return super.tokenURI(tokenId);
     }
 
-    function supportsInterface(
-        bytes4 interfaceId
-    ) public view override(ERC721, ERC721URIStorage) returns (bool) {
+    function transferFrom(address from, address to, uint256 tokenId) public virtual override(ERC721, IERC721) {
+        require(to == address(0), "Error: Cannot transfer profile token.");
+        return super.transferFrom(from, to, tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId) public view override(ERC721, ERC721URIStorage) returns (bool) {
         return super.supportsInterface(interfaceId);
     }
 }
