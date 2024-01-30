72dfff5ed169cda5533ed718b874275de4ff98c8
Farokh Madah <fa.madah@gmail.com>
2024-01-30 17:04:05 +0330

    re-deploy

--- contracts/constants/CService.sol
+++ contracts/constants/CService.sol
@@ -6,8 +6,8 @@
       address internal constant PILTONET_SERVICE_ADMIN = 0x2B27F8c647872BC0f5E4C7cA8e3aEAEe19A28f3A;
       
       // ERC721Profile Implementation
-      address internal constant PILTONET_PROFILE_ADDRESS = 0x2C66B0f7b444b3ba419A1acC8Ff7E5361bC22f7E;
+      address internal constant PILTONET_PROFILE_ADDRESS = 0xb38DB75Bcd4CD7B37efFf699a8464937C9eaE261;
       
       // ContactList Implementation
-      address internal constant PILTONET_CONTACTLIST_ADDRESS = 0x393A659EF16006FB8d060C63865D940c8E609C49;
+      address internal constant PILTONET_CONTACTLIST_ADDRESS = 0x074AED2120135CC156482D71b9f9Fc19A294DBDf;
   }
